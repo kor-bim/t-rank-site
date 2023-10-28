b5a12d3eabb3c10e9ebf91ed77cd7ec547900aa1
kor-bim <dbsgksqlschl@gmail.com>
2023-10-28 19:00:25 +0900

    Initial commit from Create Next App

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
           <div className="relative flex flex-col">
             <Navbar />
-            <main className={'w-full flex flex-col items-center justify-center py-10 gap-5 z-10'}>{children}</main>
+            <main className={'w-full h-[80dvh] flex flex-col items-center justify-center py-10 gap-5 z-10'}>
+              {children}
+            </main>
             <GradientBackground />
           </div>
         </Providers>
